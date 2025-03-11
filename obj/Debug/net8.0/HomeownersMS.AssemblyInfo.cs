2f024fc2a494d872b327b3c4c567d7383ab78902
VulpritProoze <raileyalin@gmail.com>
2025-03-11 10:55:09 +0800

    changing adminlayout and residntlayout

--- obj/Debug/net8.0/HomeownersMS.AssemblyInfo.cs
+++ obj/Debug/net8.0/HomeownersMS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HomeownersMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d23d492ce1f92fd7fd0723811558501f3415026b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7026c9f6aac97af0e04dde35269aa8665fbf19e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("HomeownersMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HomeownersMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
