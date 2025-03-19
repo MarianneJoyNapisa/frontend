b5080446a3c0ef9c3dd183b8268775bee264032e
VulpritProoze <raileyalin@gmail.com>
2025-03-20 07:55:30 +0800

    Addded static pages: Community, Service, Resources

---
+++ Pages/Community/Community.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+
+namespace HomeownersMS.Pages.Community
+{
+    public class CommunityModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Service/Service.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+
+namespace HomeownersMS.Pages.Service
+{
+    public class ServiceModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
